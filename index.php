85762cbe02e4019c20ea104874f35630d33e6fe7
Vladimir Nimchyn <nima.vova@gmail.com>
2016-10-23 03:30:02 +0300

    add form input string

--- index.php
+++ index.php
@@ -10,10 +10,17 @@
 require __DIR__ . '/vendor/autoload.php';
 use Cocur\Slugify\Slugify;
 $slugify = new Slugify();
-echo $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
+//echo $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
+  if (!empty($_POST['name']) ){
+      echo $slugify->slugify($_POST['name']);
+      unset($_POST);
+}
 ?>
 
-
+<form action="index.php" method="post">
+    <p>введите строку <input type="text" name="name" /></p>
+    <p><input type="submit" /></p>
+</form>
 
 </body>
 </html>
