205439ca5c8969da4a8bc07e67dc0411f9ebabbd
Vladimir Nimchyn <nima.vova@gmail.com>
2016-10-23 03:11:28 +0300

    add slugify

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
 
 </head>
 <body>
+<?
+require __DIR__ . '/vendor/autoload.php';
+use Cocur\Slugify\Slugify;
+$slugify = new Slugify();
+echo $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
+?>
+
 
 
 </body>
