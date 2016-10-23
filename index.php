2d9bf5043f10d954aa25760457413163064f44e3
Vladimir Nimchyn <nima.vova@gmail.com>
2016-10-23 11:47:06 +0300

    first rebrending final index.php

--- index.php
+++ index.php
@@ -6,41 +6,24 @@
 
 </head>
 <body>
-<?
-<<<<<<< HEAD
-require __DIR__ . '/vendor/autoload.php';
-use Cocur\Slugify\Slugify;
-$slugify = new Slugify();
-//echo $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
-$name=$_POST['name'];
-if (isset($name) ){
-    echo $slugify->slugify($name);
-      //unset($name);
-}
-else {
-
-    unset($name);
-}
-?>
-
 <form action="index.php" method="post">
     <p>введите строку <input type="text" name="name" /></p>
     <p><input type="submit" /></p>
 </form>
-=======
-//phpinfo();
-//require __DIR__ . '/vendor/autoload.php';
+<?
 
-//require "vendor/autoload.php";
+//require __DIR__ . '/vendor/autoload.php';
 require 'vendor/autoload.php';
 
-use Carbon\Carbon;
+  use Cocur\Slugify\Slugify;
+  $slugify = new Slugify();
+  $name=$_POST['name'];
+  echo $slugify->slugify($name)."<br>";
+      //unset($name);
 
-printf("Now: %s", Carbon::now());
-
-
+  use Carbon\Carbon;
+  printf("Now: %s", Carbon::now());
 ?>
->>>>>>> time
 
 </body>
 </html>
